14d216a67fde661930ba66767d768f6cb78946dd
Alan Albuquerque <albuquerquealan03@gmail.com>
2024-02-28 00:27:00 +0530

    Version 2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,56 @@
-document.getElementById("ordersBtn").addEventListener("click" , function(){
-    const boxContainer=document.createElement('div');
-    boxContainer.className="box-container";
+document.getElementById("ordersBtn").addEventListener("click", function(){
 
-    const boxContent=document.createElement('div');
-    boxContent.className="box-content";
+    // let latestOrderNo=1;
 
-    const content =`<p>Order No</p>
-    <p>Name</p>
-    <p>Usn</p>
-    <div class="file-container">
-        <p>File</p>
-    </div>
-    <button class="accept-btn btn1">Accept</button>
-    <button class="reject-btn btn1">Reject</button>`;
-    boxContent.innerHTML=content;
+    // function generateOrderNo() {
+    //     return latestOrderNo++;
+    // }
 
+    // const orderNo =generateOrderNo();
+    
+    const sampleOrder = {
+        orderNo: "1",
+        name: "Alan Albuquerque",
+        usn: "1DT21CS012",
+        file: "example.pdf",
+        description: "This is a sample description."
+    };
+
+    
+    const boxContainer = document.createElement('div');
+    boxContainer.className = "box-container";
+
+    const boxContent = document.createElement('div');
+    boxContent.className = "box-content";
+
+    
+    const content = `
+    <h2>Order No: ${sampleOrder.orderNo}</h2>
+    <p>Name: ${sampleOrder.name}</p>
+    <p>USN: ${sampleOrder.usn}</p>
+    <p><a href="${sampleOrder.file}" id="fileLink"><img src="img/file-img.png" alt="File Image" class="file-image"></a></p> 
+    <p>Description: ${sampleOrder.description}</p>
+    <div class="action-images">
+        <img src="img/accept.png" alt="Accept Image" class="action-image accept-image"> 
+        <img src="img/reject.png" alt="Reject Image" class="action-image reject-image">
+    </div>`;
+
+    boxContent.innerHTML = content;
+
+    
     boxContainer.appendChild(boxContent);
 
+    
     document.getElementById("cardContainer").appendChild(boxContainer);
+
+    const acceptMessage=document.querySelector('.accept-image');
+    const rejectMessage=document.querySelector('.reject-image');
+
+    acceptMessage.addEventListener('click', function() {
+        console.log('Accepted');
+    });
+
+    rejectMessage.addEventListener('click', function(){
+        console.log('Rejected');
+    })
 });
