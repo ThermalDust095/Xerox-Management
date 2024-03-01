c4549a53619052b7a9fe74ecc41cd48bd0757ed7
ThermalDust095 <79693586+ThermalDust095@users.noreply.github.com>
2024-03-01 23:56:41 +0530

    done.

--- web.js
+++ web.js
@@ -21,6 +21,6 @@ app.get("/client", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
